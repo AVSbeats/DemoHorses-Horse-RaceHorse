5495757929b65b827fb4f283daeabc9e5c8d43ee
Abraham Vallejos-Soto <125098951+AVSbeats@users.noreply.github.com>
2023-03-24 20:54:07 -0400

    First to main
    Started!

---
+++ Horse.java
@@ -0,0 +1,30 @@
+public class Horse { // Superclass
+
+    String name;
+    String color;
+    int birthYear;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+}
---
+++ RaceHorse.java
@@ -0,0 +1,13 @@
+public class RaceHorse extends Horse { // Subclass
+
+    int races;
+
+    public void setRaces(int races) {
+        this.races = races;
+    }
+
+    public int getRaces() {
+        return races;
+    }
+
+}
